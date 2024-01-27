df02e6f626cf9f90a9f905fbc99ae228b7b4c9ac
IvanBrat <dredd0077@gmail.com>
2024-01-27 20:44:50 +0300

    test

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace magazine
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
---
+++ Window1.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace magazine
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
